6682496fe551b4296ddc9a3a07a33d782e1b6e8b
ibrahimfardeen <ibrahimfardeen.n@gmail.com>
2024-01-23 22:56:55 +0530

    All api's changed

--- app/api/getdatabasedetectives/route.js
+++ app/api/getdatabasedetectives/route.js
@@ -2,11 +2,14 @@ import connectDB from "@/app/lib/mongodb";
 import Contact from "@/app/models/contact";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
-import { authorize } from "@/app/lib/authorization";
 export async function GET() {
   try {
-    var authorized = await authorize();
-    if(!authorized){
+    const allowedIp = ['49.37.209', '192.168.1','192.168.0','223.187.115']; 
+    var response = await fetch('https://api.ipify.org?format=json');
+    var data = await response.json();
+    var ip = data.ip.split(".",3).toString().replaceAll(',','.');
+    console.log(ip);
+    if(!allowedIp.includes(ip)){
       return NextResponse.json({
         msg:"You are not authorized to view this data",
         data: []
