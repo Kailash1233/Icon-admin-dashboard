c5591d325c3f6c9839168ae2928e4bd7a5ac7a01
Mohammed ibrahim fardeen <106424022+ibrahimfardeen@users.noreply.github.com>
2024-02-07 19:27:27 +0530

    Console added

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ export default function Home() {
       }),
     });
     const response = await res.json();    
-    // console.log(JSON.stringify(response.data))
+     console.log(JSON.stringify(response.data))
     if(eventname == '' && response.data.length > 0){
       setAlldata(true)
       setData(response.data);
